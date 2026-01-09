60e9f4d4c4333fcba46bd8c4fba80a1c9252cf8c
Aschalew Daraje <aschalewdere2@gmail.com>
2026-01-09 05:10:26 -0800

    calling with aproprate middileware

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
 require('dotenv').config();
 const express = require('express');
 const connectDB = require('./config/db');
+const authRoutes = require('./routes/authRoutes');
 
 const app = express();
 
 connectDB();
 
 app.use(express.json());
+
+// Auth routes
+app.use('/api/auth', authRoutes);
 
 app.get('/', (req, res) => {
   res.status(200).json({ status: 'ok' });
