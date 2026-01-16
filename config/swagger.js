c7407a2062fa958351999a6ec553f98001e2609d
Aschalew Daraje <aschalewdere2@gmail.com>
2026-01-16 07:58:24 -0800

    swagger configration

---
+++ config/swagger.js
@@ -0,0 +1,32 @@
+const swaggerJSDoc = require('swagger-jsdoc');
+
+const swaggerDefinition = {
+  openapi: '3.0.0',
+  info: {
+    title: 'Furniture Marketplace API',
+    version: '1.0.0',
+  },
+  servers: [
+    {
+      url: 'http://localhost:5000',
+    },
+  ],
+  components: {
+    securitySchemes: {
+      BearerAuth: {
+        type: 'http',
+        scheme: 'bearer',
+        bearerFormat: 'JWT',
+      },
+    },
+  },
+};
+
+const options = {
+  swaggerDefinition,
+  apis: ['./routes/*.js', './controllers/*.js'],
+};
+
+const swaggerSpec = swaggerJSDoc(options);
+
+module.exports = swaggerSpec;
